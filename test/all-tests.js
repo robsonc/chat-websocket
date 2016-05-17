c5c64056eb4c87094231346a7f3a7f97d99095af
Robson Coutinho <robson_coutinho@yahoo.com.br>
2016-05-17 09:24:01 -0300

    added tests

---
+++ test/all-tests.js
@@ -0,0 +1,8 @@
+var chai = require('chai');
+var expect = chai.Expect();
+
+describe('Test WebSocket App', function(){
+    it('should pass', function(){
+        expect(true).to.be.equal(true);    
+    });
+});
